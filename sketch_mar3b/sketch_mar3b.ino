6cd180fd00a31bc53820ab2a390e6fe0af9d1693
Eldon <Birdheldon@gmail.com>
2025-03-04 01:38:54 -0700

    almost there
    I'm almost done completly, the only real problems I have left are getting it to run on the dang thing, then building a completed module for production.

--- sketch_mar3b/sketch_mar3b.ino
+++ sketch_mar3b/sketch_mar3b.ino
@@ -1,45 +1,79 @@
-#include <cstring>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
-struct {
 
-  int8_t module_id[6];
-  int8_t user_auth[8];
+// a packet will also include the mac address
+typedef struct {
+
+  byte module_id[6];
+  byte user_auth[8];
 
   float heat;
   float moisture;
 
 } Packet_t;
 
-
-int8_t self_id[6];
-int8_t user_id[8];
-
-String domain;
-String wifipass;
-String router;
-
-WiFiClient client;
-
 byte mac[6];
 
 
+byte self_id[6];
+byte user_id[8];
+
+const char* domain = "http://181.214.153.123:52067";
+const char* pass = "onmyhonor";
+const char* router = "optix";
+
+WiFiClient client;
+
+// in the future for this function, I want it to be able to ask for a id, and then self assign itself with a unique identifier
 
 
-// this is for assigning yourself a user id and password
-int handshake(string domain){
+// int handshake(String domain){
 
-  // initiate handshake with server, ask for assignmnet of user id and new module id;
+//   // initiate handshake with server, ask for assignmnet of user id and new module id;
 
+//   if(WiFi.status() == WL_CONNECTED){
 
+//     HTTPClient http;
 
+//     String mac_str = "";
   
+//     for (int i = 0; i < 6; i++){
+//       if(mac[i] < 0x10){ mac_str += "0"; }
+//       mac_str += String(mac[i], HEX);
+//       if(i < 5){
+//         mac_str += ":";
+//       }
+//     }
+
+
+    
+//   }
+
+//   return 0;
+// }
+
+String bytes_to_string(byte* array, int length){
+
+  String result = "";
+  for(int i = 0; i < length; i++){
+    if(array[i] < 0x10){
+      result += "0";
+    }
+
+    result += String(array[i], HEX);
+
+    if(i < length - 1){
+      result += ":";
+    }
+  }
+
+  return result;
 }
 
 
-int send_packet(Packet_t packet){
+int transmit_packet(Packet_t packet){
 
   // send a network packet to the server to be interpreted
   // return true if sucessful...
@@ -51,51 +85,109 @@ int send_packet(Packet_t packet){
     http.begin(client, domain);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    String request = ("temp=" + String())
+    String request_data = ("module_id=" + bytes_to_string(packet.module_id, 6) + 
+                           "&user_auth="+ bytes_to_string(packet.user_auth, 8) + 
+                           "&heat=" + String(packet.heat) + 
+                           "&moisture=" + String(packet.moisture) + 
+                           "&mac=" + bytes_to_string(mac, 6));
 
-  }
-  else{
+    int http_code = http.POST(request_data);
+
+    if(http_code == HTTP_CODE_OK){
+      String payload = http.getString();
+      http.end();
+      return 0;
+    }
+    else{
+    http.end();
+      return 2;
+    }
+  } else{
     // this fires if not connected to the WiFi using the client
     return 1;
   }
 
 
-
-  return 0;
+  return 1;
 }
+
 
 
 void setup(){
 
   Serial.begin(115200);
-  Wifi.begin(router, wifipass);
 
+  // set pin modes here when they are decided... !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // testing, operations normal...
 
   // wait while we make handshake with router
-  while(WiFi.stats() != WL_CONNECTED){
 
+  WiFi.begin(router, pass);
+
+  // dht.begin();
+
+
+  while(WiFi.status() != WL_CONNECTED){
     delay(1000);
-
   }
 
   // This only runs when we are connected to the wifi on the router...
 
-  // Set pin mode for the sensors.
-
   WiFi.macAddress(mac);
 
 
+  // This is basically saying, if I have an assigned ID, dont get another one from the server, but if this is the first time this module is running, then grab a unique id.
+
+  // if(module_id == Null){
+  //   int handshake_out = handshake();
+
+  //   if(handshake_out == 0){
 
 
+    
+  //   } 
+  // }
 
-  return 0;
   
-
-
-
+  
 }
 
 
 void loop(){
 
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Packet_t working_packet = {
+    .module_id = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06},
+    .user_auth = {0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6, 0xA7, 0xB8},
+
+    .heat = 25.5,
+    .moisture = 72.3
+  };
+
+  int result = transmit_packet(working_packet);
+  if(result == 0){
+    // successfull packet transmition
+
+
+  } else{
+
+    // on failure, blink a few times to make the idea clear
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+  }
+
+
+  // wait this duration between sending transmitions, only need them once per our in production...
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
 }
