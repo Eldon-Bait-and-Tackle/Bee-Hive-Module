673ab2e8afbbd33087f2b1c91d857b0451c79713
Eldon <56803340+EldonBird@users.noreply.github.com>
2025-03-06 00:27:45 -0700

    BLOWING MY BRAINS OUT!!!

--- sketch_mar3b/sketch_mar3b.ino
+++ sketch_mar3b/sketch_mar3b.ino
@@ -3,7 +3,6 @@
 #include <WiFiClient.h>
 
 
-// a packet will also include the mac address
 typedef struct {
 
   byte module_id[6];
@@ -14,45 +13,23 @@ typedef struct {
 
 } Packet_t;
 
+
+
 byte mac[6];
-
-
 byte self_id[6];
 byte user_id[8];
 
-const char* domain = "http://181.214.153.123:52067";
-const char* pass = "onmyhonor";
-const char* router = "optix";
+
+
+const char* domain = "http://181.214.153.183";
+const char* pass = "12345678";
+const char* router = "SADIE";
+
+
 
 WiFiClient client;
 
-// in the future for this function, I want it to be able to ask for a id, and then self assign itself with a unique identifier
 
-
-// int handshake(String domain){
-
-//   // initiate handshake with server, ask for assignmnet of user id and new module id;
-
-//   if(WiFi.status() == WL_CONNECTED){
-
-//     HTTPClient http;
-
-//     String mac_str = "";
-  
-//     for (int i = 0; i < 6; i++){
-//       if(mac[i] < 0x10){ mac_str += "0"; }
-//       mac_str += String(mac[i], HEX);
-//       if(i < 5){
-//         mac_str += ":";
-//       }
-//     }
-
-
-    
-//   }
-
-//   return 0;
-// }
 
 String bytes_to_string(byte* array, int length){
 
@@ -112,7 +89,6 @@ int transmit_packet(Packet_t packet){
 }
 
 
-
 void setup(){
 
   Serial.begin(115200);
@@ -120,46 +96,46 @@ void setup(){
   // set pin modes here when they are decided... !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // testing, operations normal...
 
   // wait while we make handshake with router
 
+  Serial.print("Connecting to WiFi network: ");
+  Serial.println(router);
+
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(router, pass);
 
-  // dht.begin();
+  WiFi.beginSmartConfig();
 
+  int timeout = 10;
 
-  while(WiFi.status() != WL_CONNECTED){
+  while(WiFi.status() != WL_CONNECTED && timeout > 0){
+
+    digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print(".");
+
+    timeout--;
   }
+
+  if(WiFi.status() == WL_CONNECTED){
+    WiFi.macAddress(mac); 
+    Serial.println("WiFi connected successfully!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("MAC address: ");
+    Serial.println(bytes_to_string(mac, 6));
+  }
+  
 
   // This only runs when we are connected to the wifi on the router...
 
-  WiFi.macAddress(mac);
 
-
-  // This is basically saying, if I have an assigned ID, dont get another one from the server, but if this is the first time this module is running, then grab a unique id.
-
-  // if(module_id == Null){
-  //   int handshake_out = handshake();
-
-  //   if(handshake_out == 0){
-
-
-    
-  //   } 
-  // }
-
-  
-  
 }
 
-
-void loop(){
-
-  digitalWrite(LED_BUILTIN, LOW);
-
-  Packet_t working_packet = {
+Packet_t working_packet = {
     .module_id = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06},
     .user_auth = {0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6, 0xA7, 0xB8},
 
@@ -167,27 +143,49 @@ void loop(){
     .moisture = 72.3
   };
 
+
+void loop(){
+
+  Serial.println("TEST");
+
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("WiFi has magically connected");
+  }
+
+
   int result = transmit_packet(working_packet);
   if(result == 0){
     // successfull packet transmition
 
 
+  } else if (result == 1){
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      
   } else{
-
-    // on failure, blink a few times to make the idea clear
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+    
   }
 
 
   // wait this duration between sending transmitions, only need them once per our in production...
 
-  digitalWrite(LED_BUILTIN, HIGH);
   delay(2000);
 }
